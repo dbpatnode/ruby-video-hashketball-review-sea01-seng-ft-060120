3787f8125875ced22346e6ad786d0780ad1718d5
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-20 19:12:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -170,8 +170,8 @@ end
 def team_names
   names = nil
   game_hash.each do |home_away_hash, team_information_hash|
-  names = team_information_hash[:team_name].flatten
- binding.pry
+  names = team_information_hash[:team_name]
+# binding.pry
   end
   names
 end
