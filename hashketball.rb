e8869fff2fc7e3d4a61cae49e5dfee5b74f5f686
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-20 19:32:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,9 +168,9 @@ end
 
 # Build a method, team_names, that operates on the game Hash to return an Array of the team names.
 def team_names
-  names = nil
+  names = []
   game_hash.each do |home_away_hash, team_information_hash|
-  names = team_information_hash[:team_name]
+  names = team_information_hash[:team_name].flatten
   #binding.pry
   end
   names
